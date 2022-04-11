334a4c1c540fcacfeb2958bf9bb762560e14ca06
prakhar-glitch <54439226+prakhar-glitch@users.noreply.github.com>
2022-04-11 18:41:07 +0530

    Styled the color-boxes

--- color-picker/src/ColorBox.jsx
+++ color-picker/src/ColorBox.jsx
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import "./ColorBox.css";
+
 export default class ColorBox extends Component {
   render() {
+    const { name, color } = this.props.bg;
     return (
-      <div
-        className="color-box"
-        style={{ backgroundColor: this.props.bg.color }}
-      >
-        {this.props.bg.name}
-        <span>More</span>
+      <div className="color-box" style={{ background: color }}>
+        <div className="color-container">
+          <div className="box-content">
+            <span> {name}</span>
+          </div>
+          <button className="copy-button">Copy</button>
+        </div>
+        <spna className="see-more">More</spna>
       </div>
     );
   }
