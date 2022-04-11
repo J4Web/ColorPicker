c1c96a35b70c0d737fbc4349ca3646a94a9a99e0
prakhar-glitch <54439226+prakhar-glitch@users.noreply.github.com>
2022-04-11 18:49:33 +0530

    https://github.com/nkbt/react-copy-to-clipboard used and just wrap whereever you wanna click to copy and just pass in the color

--- color-picker/src/ColorBox.jsx
+++ color-picker/src/ColorBox.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 import "./ColorBox.css";
-
+import { CopyToClipboard } from "react-copy-to-clipboard";
 export default class ColorBox extends Component {
   render() {
     const { name, color } = this.props.bg;
     return (
-      <div className="color-box" style={{ background: color }}>
-        <div className="color-container">
-          <div className="box-content">
-            <span> {name}</span>
+      <CopyToClipboard text={color}>
+        <div className="color-box" style={{ background: color }}>
+          <div className="color-container">
+            <div className="box-content">
+              <span> {name}</span>
+            </div>
+            <button className="copy-button">Copy</button>
           </div>
-          <button className="copy-button">Copy</button>
+          <spna className="see-more">More</spna>
         </div>
-        <spna className="see-more">More</spna>
-      </div>
+      </CopyToClipboard>
     );
   }
 }
