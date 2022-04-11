d8e9d5251ac6cc0806ab3c38e37545681b6b2548
prakhar-glitch <54439226+prakhar-glitch@users.noreply.github.com>
2022-04-11 19:38:36 +0530

    added the overlay animation

--- color-picker/src/ColorBox.jsx
+++ color-picker/src/ColorBox.jsx
@@ -2,11 +2,32 @@ import React, { Component } from "react";
 import "./ColorBox.css";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 export default class ColorBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      copied: false,
+    };
+    this.handleCopyToClipboard = this.handleCopyToClipboard.bind(this);
+  }
+  handleCopyToClipboard() {
+    this.setState({ copied: true }, () => {
+      setTimeout(() => this.setState({ copied: false }), 1500);
+    });
+  }
   render() {
     const { name, color } = this.props.bg;
+    const { copied } = this.state;
     return (
-      <CopyToClipboard text={color}>
+      <CopyToClipboard text={color} onCopy={this.handleCopyToClipboard}>
         <div className="color-box" style={{ background: color }}>
+          <div
+            style={{ background: color }}
+            className={`color-box-overlay ${copied && "show"}`}
+          ></div>
+          <div className={`copy-msg ${copied && "show"}`}>
+            <h1>copied</h1>
+            <p>{color}</p>
+          </div>
           <div className="color-container">
             <div className="box-content">
               <span> {name}</span>
