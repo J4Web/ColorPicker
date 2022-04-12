463764171cb9e8f526eb561ddde8c893871c1f4c
prakhar-glitch <54439226+prakhar-glitch@users.noreply.github.com>
2022-04-12 06:13:14 +0530

    added the slider and its working

--- color-picker/src/Palette.jsx
+++ color-picker/src/Palette.jsx
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
-
+import Slider, { Range } from "rc-slider";
+import "rc-slider/assets/index.css";
 import "./Palette.css";
 class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { level: 500 };
+    this.changeLevel = this.changeLevel.bind(this);
+  }
+  changeLevel(newlevel) {
+    this.setState({ level: newlevel });
+  }
   render() {
-    const boxes = this.props.palette.colors[300].map((box) => {
-      console.warn(box);
+    const { level } = this.state;
+    // console.error(level);
+    const boxes = this.props.palette.colors[level].map((box) => {
+      // console.warn(box);
       return <ColorBox bg={box} />;
     });
     return (
       <div className="Palette">
+        <Slider
+          defaultValue={this.state.level}
+          min={100}
+          max={900}
+          step={100}
+          onAfterChange={this.changeLevel}
+        />
         {/* navbar */}
         <div className="Palette-colors">{boxes}</div>
         {/* footer */}
