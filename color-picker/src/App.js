6941a3186aa40ee4019f0382a6ffcc2e45abe544
prakhar-glitch <54439226+prakhar-glitch@users.noreply.github.com>
2022-04-13 07:25:00 +0530

    added my routes to my palette

--- color-picker/src/App.js
+++ color-picker/src/App.js
@@ -2,12 +2,19 @@ import SeedColors from "./SeedColors";
 import Palette from "./Palette";
 import "./App.css";
 import { getPalette } from "./ColorHelpers";
-
+import { Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
-    <div>
-      <Palette palette={getPalette(SeedColors[4])} />
-    </div>
+    <Routes>
+      <Route
+        path=""
+        element={<h1>Hellooo just testing this home page</h1>}
+      ></Route>
+      <Route
+        path="/palette/:id"
+        element={<Palette palette={getPalette(SeedColors[4])} />}
+      />
+    </Routes>
   );
 }
 
