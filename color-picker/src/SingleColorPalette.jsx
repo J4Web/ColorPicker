7ceb2ebe9ee46629956322594ff0b3b0e2283cd0
prakhar-glitch <54439226+prakhar-glitch@users.noreply.github.com>
2022-04-24 23:50:28 +0530

    adding the method to gather the shades of the clicked color

--- color-picker/src/SingleColorPalette.jsx
+++ color-picker/src/SingleColorPalette.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import { WithRoutes } from "./WithRoutes";
 import SeedColors from "./SeedColors";
+import { getPalette } from "./ColorHelpers";
 class SingleColorPalette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   findPalette = (id) => {
     // console.log(id);
     return SeedColors.find(function (palette) {
@@ -9,8 +14,10 @@ class SingleColorPalette extends Component {
     });
   };
   render() {
-    const { colorId } = this.props.params;
+    const { colorId, paletteId } = this.props.params;
+    const palette = getPalette(this.findPalette(paletteId));
     console.log(colorId);
+    console.log(palette);
     return (
       <div>
         <h2>Inside Color Palette</h2>
