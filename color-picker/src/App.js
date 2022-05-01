ac4480c51ee0b13287eef1da086bc88aaee73ca2
prakhar-glitch <54439226+prakhar-glitch@users.noreply.github.com>
2022-05-02 03:40:42 +0530

    made local storage work and it updates the storage only when palette state changes using UseEffect

--- color-picker/src/App.js
+++ color-picker/src/App.js
@@ -7,11 +7,16 @@ import SingleColorPalette from "./SingleColorPalette";
 import NewPaletteForm from "./NewPaletteForm";
 import { useState, useEffect } from "react";
 function App() {
-  const [palette, setPalette] = useState(SeedColors);
+  const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
+  const [palette, setPalette] = useState(savedPalettes || SeedColors);
   const savePalette = (newPalette) => {
     // console.log(newPalette);
     setPalette([...palette, newPalette]);
   };
+  useEffect(() => {
+    console.log(palette, "suxhsdxnjnsjdcnjn");
+    window.localStorage.setItem("palettes", JSON.stringify(palette));
+  }, [palette]);
 
   return (
     <Routes>
